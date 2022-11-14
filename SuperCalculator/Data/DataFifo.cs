ec2ebb78daace7770ec02a3a18e899767dba5e9a
Tóth Tibor <toth.tibor@autsoft.hu>
2022-11-14 14:13:35 +0100

    lock

--- SuperCalculator/Data/DataFifo.cs
+++ SuperCalculator/Data/DataFifo.cs
@@ -6,27 +6,34 @@ namespace SuperCalculator.Data;
 public class DataFifo
 {
     private readonly List<double[]> _innerList = new List<double[]>();
+    private object _syncRoot = new object();
 
     public void Put(double[] data)
     {
-        _innerList.Add(data);
+        lock (_syncRoot)
+        {
+            _innerList.Add(data); 
+        }
     }
 
     public bool TryGet(out double[] data)
     {
-        if (_innerList.Count > 0)
+        lock (_syncRoot)
         {
-            // Some little artificial delay, which does not affect the FIFO functionality
-            Thread.Sleep(500);
+            if (_innerList.Count > 0)
+            {
+                // Some little artificial delay, which does not affect the FIFO functionality
+                Thread.Sleep(500);
 
-            data = _innerList[0];
-            _innerList.RemoveAt(0);
-            return true;
-        }
-        else
-        {
-            data = null;
-            return false;
+                data = _innerList[0];
+                _innerList.RemoveAt(0);
+                return true;
+            }
+            else
+            {
+                data = null;
+                return false;
+            } 
         }
     }
 }
